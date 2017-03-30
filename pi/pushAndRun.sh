8dddfff700da60c1ea07cbe83510f91770d3e23b
Marle <jakobrydhof@gmail.com>
2017-03-30 11:13:09 +0200

    commiting to run by pushAndRun

--- pi/pushAndRun.sh
+++ pi/pushAndRun.sh
@@ -8,7 +8,7 @@ git add .
 git commit -m 'commiting to run by pushAndRun'
 git push origin pushAndRun
 
-ssh pi@raspi 'cd IOT/pi/;git pull origin pushAndRun; git checkout pushAndRun; echo; echo;node blue.js'
+ssh pi@raspi 'cd IOT/pi/;git pull origin pushAndRun; git checkout pushAndRun > /dev/null; echo; echo;node blue.js;git checkout master > /dev/null'
 
 
 
