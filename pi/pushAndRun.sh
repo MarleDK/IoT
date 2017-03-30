4d6dd98733156da73e8f2bc2a3a8c35f7bc2385b
Marle <jakobrydhof@gmail.com>
2017-03-30 10:52:01 +0200

    commiting to run by pushAndRun

--- pi/pushAndRun.sh
+++ pi/pushAndRun.sh
@@ -4,7 +4,7 @@ git add .
 git commit -m 'commiting to run by pushAndRun'
 git push
 
-ssh pi@raspi 'git pull; node blue.js > output.txt'
+ssh pi@raspi 'git pull origin master; node blue.js > output.txt'
 
 
 
