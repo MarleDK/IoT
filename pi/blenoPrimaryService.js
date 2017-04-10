0be2d85bbb6b4a7785b6aabaa44bc5da46558a20
Marle <jakobrydhof@gmail.com>
2017-04-10 14:47:05 +0200

    commiting to run by pushAndRun

--- pi/blenoPrimaryService.js
+++ pi/blenoPrimaryService.js
@@ -1,12 +1,20 @@
 var bleno = require('bleno')
 var getConnectionChar = require('./blenoGetConnectionPuplicKeyCharacteristic')
 
+
+
 var PrimaryService = bleno.PrimaryService;
 
-var primaryService = new PrimaryService({
-    uuid: 'fffffffffffffffffffffffffffffff0', // or 'fff0' for 16-bit
-    characteristics: [
-        // see Characteristic for data type
-        getConnectionChar
-    ]
-});
+
+function BatteryService() {
+    BatteryService.super_.call(this, {
+        uuid: 'fffffffffffffffffffffffffffffff0', // or 'fff0' for 16-bit
+        characteristics: [
+            // see Characteristic for data type
+            getConnectionChar
+        ]
+    });
+}
+
+
+module.exports = BatteryService;
