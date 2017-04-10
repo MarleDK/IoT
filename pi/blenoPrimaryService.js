b1fac9b94fc81108a5b0b0c3cc0834751328d748
Marle <jakobrydhof@gmail.com>
2017-04-10 14:57:43 +0200

    commiting to run by pushAndRun

--- pi/blenoPrimaryService.js
+++ pi/blenoPrimaryService.js
@@ -7,7 +7,7 @@ var PrimaryService = bleno.PrimaryService;
 
 
 class BatteryService{
-    uuid: 'fffffffffffffffffffffffffffffff0', // or 'fff0' for 16-bit
+    uuid= 'fffffffffffffffffffffffffffffff0', // or 'fff0' for 16-bit
     characteristics: [
         // see Characteristic for data type
         new getConnectionChar
