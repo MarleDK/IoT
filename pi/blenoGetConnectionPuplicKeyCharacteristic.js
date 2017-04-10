936aa16046692f8def099c061fbec4442b6a52be
Marle <jakobrydhof@gmail.com>
2017-04-10 15:40:32 +0200

    commiting to run by pushAndRun

--- pi/blenoGetConnectionPuplicKeyCharacteristic.js
+++ pi/blenoGetConnectionPuplicKeyCharacteristic.js
@@ -14,10 +14,10 @@ function readRequestFunction(offset,callback){
 
 class Characteristic{
   constructor(){
-    this.uuid= 'fffffffffffffffffffffffffffffff1', // or 'fff1' for 16-bit
-    this.properties= [ 'read' ], // can be a combination of 'read', 'write', 'writeWithoutResponse', 'notify', 'indicate'
-    this.onReadRequest= readRequestFunction // optional read request handler, function(offset, callback) { ... }
-
+    this.uuid = 'fffffffffffffffffffffffffffffff1', // or 'fff1' for 16-bit
+    this.properties = [ 'read' ], // can be a combination of 'read', 'write', 'writeWithoutResponse', 'notify', 'indicate'
+    this.onReadRequest = readRequestFunction // optional read request handler, function(offset, callback) { ... }
+    this.secure = []
   }
 };
 
