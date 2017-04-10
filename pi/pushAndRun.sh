687b7f7d7ccae9268ef4c003de1dfdc590387df1
Marle <jakobrydhof@gmail.com>
2017-04-10 12:54:27 +0200

    commiting to run by pushAndRun

--- pi/pushAndRun.sh
+++ pi/pushAndRun.sh
@@ -8,4 +8,4 @@ git add .
 git commit -m 'commiting to run by pushAndRun'
 git push origin pushAndRun
 
-ssh pi@raspi 'cd ~/IOT/pi/;git pull origin pushAndRun; git checkout -q pushAndRun; echo; echo;node blue.js 2> error.log;git checkout -q master;echo "Following errors occured: ";cat error.log'
+ssh pi@raspi 'cd ~/IOT/pi/;git pull origin pushAndRun; git checkout -q pushAndRun; echo; echo;node blue.js 2> error.log;echo "Following errors occured: ";cat error.log'
