b52306d39092c58f306302ec5bd372f80de435ac
Marle <jakobrydhof@gmail.com>
2017-04-17 16:15:23 +0200

    commiting to run by pushAndRun

--- pi/blenoGetConnectionPuplicKeyCharacteristic.js
+++ pi/blenoGetConnectionPuplicKeyCharacteristic.js
@@ -5,19 +5,19 @@ function encrypt(key) {
   return (5*key)
 }
 
-function readRequestFunction(offset,callback){
-  console.log("read requested")
-  var publicKey = Math.pow(10,10)*Math.random();
-  var privateKey = encrypt(publicKey);
-  fs.appendFile('./DB', privateKey+'\n')
-  callback(this.RESULT_SUCCESS, new Buffer([publicKey]) )
-}
 
 class Characteristic{
   constructor(){
     this.uuid = '00007ab1-0000-1000-8000-00805f9b34fb', // or 'fff1' for 16-bit
     this.properties = [ 'read' ], // can be a combination of 'read', 'write', 'writeWithoutResponse', 'notify', 'indicate'
-    this.onReadRequest = readRequestFunction // optional read request handler, function(offset, callback) { ... }
+    this.onReadRequest = function (offset,callback){
+        console.log("read requested")
+        var publicKey = Math.pow(10,10)*Math.random();
+        var privateKey = encrypt(publicKey);
+        fs.appendFile('./DB', privateKey+'\n')
+        callback(this.RESULT_SUCCESS, new Buffer([publicKey]) )
+      }
+ // optional read request handler, function(offset, callback) { ... }
     this.secure = []
     this.descriptors = []
   }
