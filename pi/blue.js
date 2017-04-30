e79ea868cc5a1a69da1c94531de4cd76ef5f956f
Marle <jakobrydhof@gmail.com>
2017-04-30 14:11:58 +0200

    commiting to run by pushAndRun

--- pi/blue.js
+++ pi/blue.js
@@ -1,7 +1,7 @@
 var bleno = require('bleno')
 var BlenoPrimaryService = bleno.PrimaryService
 var PuplicKeyCharacteristic = require('./blenoGetConnectionPuplicKeyCharacteristic')
-var WriteCharacteristic = require('./blenoWriteToPiCharacteristic')
+//var WriteCharacteristic = require('./blenoWriteToPiCharacteristic')
 
 var primaryService = new BlenoPrimaryService({
   uuid : '00007ab0-0000-1000-8000-00805f9b34fb', // or 'fff0' for 16-bit
