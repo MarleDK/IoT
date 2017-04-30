7e913d371e9bd8f54e3de5c17fb261ac4f9413bd
Marle <jakobrydhof@gmail.com>
2017-04-30 11:10:07 +0200

    commiting to run by pushAndRun

--- pi/blenoWriteToPiCharacteristic.js
+++ pi/blenoWriteToPiCharacteristic.js
@@ -1,3 +1,5 @@
+//blenoWriteToPiCharacteristic.js
+
 var util = require('util')
 
 var bleno = require('bleno')
@@ -5,16 +7,12 @@ BlenoCharacteristic = bleno.Characteristic
 
 fs = require('fs')
 
-function encrypt(key) {
-  return (5*key)
-}
-
 
 var Characteristic = function() {
   Characteristic.super_.call(this,{
     uuid : '00007ab1-0000-1000-8000-00805f9b34fb', // or 'fff1' for 16-bit
-    properties : [ 'read' ], // can be a combination of 'read', 'write', 'writeWithoutResponse', 'notify', 'indicate'
-    onReadRequest : function (offset,callback){
+    properties : [ 'write' ], // can be a combination of 'read', 'write', 'writeWithoutResponse', 'notify', 'indicate'
+    onWriteRequest : function (offset,callback){
         console.log("read requested")
         var publicKey = Math.round(Math.pow(10,10)*Math.random());
         console.log("new public key: " + publicKey)
